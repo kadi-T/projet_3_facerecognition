e89d668c9714fd706ee235894588b1d14d69477d
kadi-T <156639209+kadi-T@users.noreply.github.com>
2024-07-06 02:11:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -74,5 +74,5 @@ async def recognition(file: UploadFile = File(...)):
     return {"results": results}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8801)
 
